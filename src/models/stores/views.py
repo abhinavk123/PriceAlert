f7c183c630b316356cc480f7d3d201fda823a4af
abhinavk123 <abhinavvv123@gmail.com>
2018-04-15 13:58:43 +0530

    Added method to edit and delete store

--- src/models/stores/views.py
+++ src/models/stores/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
 
 from src.models.stores.store import Store
 
@@ -7,12 +7,27 @@ stores_blueprint = Blueprint('stores', __name__)
 @stores_blueprint.route('/')
 def index():
     stores = Store.find_all()
-    return render_template("stores/store.jinja2",stores=stores)
+    return render_template("stores/store_index.jinja2", stores=stores)
 
-@stores_blueprint.route('/stores/<string:name>')
-def store_page(name):
-    return render_template("underconstruction.jinja2")
+@stores_blueprint.route('/store/<string:store_id>')
+def store_page(store_id):
+    store = Store.get_by_id(store_id)
+    return render_template("stores/store.jinja2",store= store)
 
-@stores_blueprint.route('/create_store')
+@stores_blueprint.route('/new',methods=['GET','POST'])
 def create_store():
-    pass
+    if request.method == 'POST':
+        pass
+
+    return "This is store creation page "
+
+@stores_blueprint.route('/edit',methods=['GET','POST'])
+def edit_store():
+    if request.method == 'POST':
+        pass
+
+    return "This is store edit page "
+
+@stores_blueprint.route('/delete/<string:store_id')
+def delete_store():
+    return "delete store"
