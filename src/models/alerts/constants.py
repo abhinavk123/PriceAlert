539b49de16262f1cfa8fe9f34e91268ecf1afb45
abhinavk123 <abhinavvv123@gmail.com>
2018-04-23 02:17:48 +0530

    Now get api details from system variables

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,7 @@
-URL = ""
-APIKEY = ""
-FROM = ""
+import os
+
+URL = os.environ.get('MAILGUN_URL')
+APIKEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
